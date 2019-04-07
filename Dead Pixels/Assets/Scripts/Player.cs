42eabd1fa025411e55f927a5b739f1c17729a235
Christopher Klewes <christopher.klewes@gmail.com>
2019-04-07 14:51:00 +0200

    Add missing meta files

--- Dead Pixels/Assets/Scripts/Player.cs
+++ Dead Pixels/Assets/Scripts/Player.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public class Player : MonoBehaviour
 {
@@ -15,9 +11,10 @@ public class Player : MonoBehaviour
     public float doubleJumpSpeed = 80f;
 
     private Rigidbody2D _body;
-    private BoxCollider2D _feetCollider;
-    
+    private Light _light;
     private Animator _animator;
+    private BoxCollider2D _feetCollider;
+    private Color _color;
 
     private bool _canJump = true; 
     
@@ -26,6 +23,8 @@ public class Player : MonoBehaviour
 
     public void Start()
     {
+        _light = GetComponent<Light>();
+        _color = _light.color;
         _body = GetComponent<Rigidbody2D>();
         _feetCollider = GetComponent<BoxCollider2D>();
         _animator = GetComponent<Animator>();
@@ -38,6 +37,40 @@ public class Player : MonoBehaviour
         Run();
         Jump();
         FlipSprite();
+        Flicker();
+    }
+
+    private void Flicker()
+    {
+        _light.color = _color * Wave();
+    }
+
+    private static float Wave() { 
+        const float amplitude = 0.2f;
+        const float frequency = 0.01f;
+        const float phase = 0.4f;
+        const float baseStart = 0.5f;
+        
+        var x = (Time.time + phase) * frequency;
+        x = x - Mathf.Floor(x);
+        var y = 1f - Random.value * 2;
+           
+        return y * amplitude + baseStart;    
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Collision with ground
+        if (other.GetContact(0).normal == Vector2.up)
+        {
+            ResetJump();
+        }
+        
+        // Collision with wall
+        else if (other.GetContact(0).normal == Vector2.right)
+        {
+            ResetJump();
+        }
     }
 
     private void ResetJump()
@@ -82,5 +115,11 @@ public class Player : MonoBehaviour
         
         var movementDirection = Mathf.Sign(_body.velocity.x);
         transform.localScale = new Vector2(movementDirection, 1f);
+    }
+
+    public void ResetPlayer()
+    {
+        
+        transform.position = new Vector3(-7, 4);
     }
 }
