3dd3ef19f001b619b8719d8e95c45f193652abd6
Christopher Klewes <christopher.klewes@gmail.com>
2019-04-07 14:48:23 +0200

    Add missing meta files

---
+++ Dead Pixels/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    public Player _player;
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Death Zone");
+        if (other.gameObject.Equals(_player.gameObject))
+        {
+            _player.ResetPlayer();
+        }
+    }
+}
